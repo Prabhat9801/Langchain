cff2f08191a3e0fb4ed43db5aa273d06755f53b2
Prabhat9801 <prabhatkumarsictc7070@gmail.com>
2025-08-30 04:05:31 +0530

    Langchain with Structure output by three methods :- 1.typeddict, 2.pydantic and 3.json_schema

--- Langchain_models/image.py
+++ Langchain_models/image.py
@@ -9,7 +9,7 @@ HF_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 # Step 1: Generate prompt with Gemini
 from langchain_google_genai import ChatGoogleGenerativeAI
 gemini = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
-prompt_result = gemini.invoke("Generate a prompt for the image generation of dog")
+prompt_result = gemini.invoke("Generate a prompt for the image generation of room which contains all the resoures of human requiremnts (include everything)")
 prompt = prompt_result.content
 print("Prompt:", prompt)
 
@@ -22,7 +22,7 @@ client = InferenceClient(
 # Output is a PIL.Image object
 image = client.text_to_image(
     prompt,
-    model="Qwen/Qwen-Image"
+    model="Qwen/Qwen-Image",
 )
 
 # Save the image
